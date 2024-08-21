6578eaa4d2989bdd976ea32fade074df472a3a7d
liudecheng <liudecheng@qq.com>
2024-08-21 14:52:11 +0800

    提交子模块

---
+++ src/components/form-designer/form.vue
@@ -0,0 +1,20 @@
+<!--
+/**
+ * author: vformAdmin
+ * email: vdpadmin@163.com
+ * website: https://www.vform666.com
+ * date: 2021.08.18
+ * remark: 如果要分发VForm源码，需在本文件顶部保留此文件头信息！！
+ */
+-->
+
+<template>
+  <div>测试组件</div>
+</template>
+
+<script>
+  export default {
+    name: "VFormDesigner",
+    componentName: "VFormDesigner",
+  }
+</script>
